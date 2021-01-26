0ca264daf4e08d35044601e89c280b9d35dbc17d
fabianpa <70101446+fabianpa@users.noreply.github.com>
2021-01-26 09:12:32 +0100

    Updated theme and added legend

---
+++ theme.js
@@ -0,0 +1,40 @@
+const colors = {
+  background: "#FFEBCB",
+  text: "313131",
+  "STEM Skills": "rgba(33, 131, 128, 0.2)",
+  "Computer Skills": "rgba(33, 131, 128, 0.3)",
+  Programming: "rgba(33, 131, 128, 0.4)",
+  "General Programming": "rgba(33, 131, 128, 0.5)",
+  "CG Programming": "rgba(33, 131, 128, 0.5)",
+  "HCI Programming": "rgba(33, 131, 128, 0.5)",
+  "Version Control": "rgba(33, 131, 128, 0.4)",
+  "Computer Usage": "rgba(33, 131, 128, 0.4)",
+  Mathematics: "rgba(33, 131, 128, 0.3)",
+  Math: "rgba(33, 131, 128, 0.5)",
+  Statistics: "rgba(33, 131, 128, 0.5)",
+  "Visual Skills": "rgba(130, 70, 112, 0.3)",
+  "Information Visualization": "rgba(130, 70, 112, 0.4)",
+  "Art and Drawing": "rgba(130, 70, 112, 0.4)",
+  "UX Evaluation": "rgba(70, 117, 153, 0.6)",
+  "Teamwork Skills": "rgba(215, 122, 97, 0.3)",
+  Communication: "rgba(215, 122, 97, 0.4)",
+  Collaboration: "rgba(215, 122, 97, 0.4)",
+
+  person: {
+    other: "#f7b538",
+    "Computer Science": "#247ba0",
+    "Media Technology": "#f25f5c"
+  },
+  border: "black"
+};
+
+const fonts = {
+  label: {
+    size: "30px",
+    weight: "400"
+  },
+  name: {
+    size: "20px",
+    weight: "400"
+  }
+}
---
+++ transform.js
@@ -0,0 +1,115 @@
+// TODO
+// host visualization online - Github pages -a
+// add the number of the skill on the bubble -a
+// change the border colour (bubbles) -a
+
+
+// skill value cutoff
+let cutoff = 5;
+
+// person skills to category
+const translation = {
+  infoVisSkills: "Information Visualization",
+  statisticSkills: "Statistics",
+  mathSkills: "Math",
+  artSkills: "Art and Drawing",
+  computerSkills: "Computer Usage",
+  programmingSkills: "General Programming",
+  compGraphProgSkills: "CG Programming",
+  HCIprogSkills: "HCI Programming",
+  UXEvalSkills: "UX Evaluation",
+  communicationSkills: "Communication",
+  collaborationSkills: "Collaboration",
+  codeRepoSkills: "Version Control",
+};
+
+const makePerson = (person, value, cutoff) => ({
+  name: person.alias,
+  value: (value - cutoff + 1) * 3,
+  color: colors.person[person.major] || colors.person.other,
+  data: { ...person },
+});
+
+const filterBySkill = (person, cutoff) =>
+  Object.entries(person).filter(
+    ([k, v]) => k.includes("Skills") && v >= cutoff
+  );
+
+const loadData = () => {
+  // list of all categories
+  let categories = Object.values(translation).reduce(
+    (acc, name) => ({
+      ...acc,
+      [name]: {
+        name,
+        children: [],
+      },
+    }),
+    {}
+  );
+
+  // place each person into relevant categories
+  people.forEach((person) => {
+    const relSkills = filterBySkill(person, cutoff);
+    relSkills.forEach(([skill, val]) => {
+      const category = translation[skill];
+      categories[category].children.push(makePerson(person, val, cutoff));
+    });
+  });
+
+  categories = Object.entries(categories)
+    .filter(([_, v]) => v.children.length > 0)
+    .reduce((acc, [k,v]) => ({ ...acc, [k]: v }), {});
+
+  // bubble hierarchy
+  const basicChart = {
+    name: "main",
+    children: [
+      {
+        name: "STEM Skills",
+        children: [
+          {
+            name: "Computer Skills",
+            color: colors["Computer Skills"],
+            children: [
+              {
+                name: "Programming",
+                children: [
+                  categories["General Programming"],
+                  categories["CG Programming"],
+                  categories["HCI Programming"],
+                ],
+              },
+              categories["Version Control"],
+              categories["Computer Usage"],
+            ],
+          },
+          {
+            name: "Mathematics",
+            children: [categories["Math"], categories["Statistics"]],
+          },
+        ],
+      },
+      categories["UX Evaluation Skills"],
+      {
+        name: "Visual Skills",
+        children: [
+          categories["Information Visualization"],
+          categories["Art and Drawing"],
+        ],
+      },
+      {
+        name: "Teamwork Skills",
+        children: [categories["Communication"], categories["Collaboration"]],
+      },
+    ],
+  };
+
+  const applyColors = (chart) => ({
+    color: colors[chart.name] || "white",
+    ...chart,
+    ...(chart.children ? { children: chart.children.filter(c => c !== undefined).map(applyColors) } : {}),
+  });
+
+  return applyColors(basicChart);
+};
